386ad5235d57755cb500a604764cd19bebf8eacf
issasalman <issasalmanhu1996@gmail.com>
2021-09-16 15:31:40 +0300

    initializing files

---
+++ server.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const express = require("express");
+const app = express();
+const cors = require("cors");
+// const mongoose = require("mongoose");
+require("dotenv").config();
+app.use(cors());
+app.use(express.json());
+
+// const PORT = process.env.PORT;
+// const MONGO_URL = process.env.MONGO_URL;
+// const DB_NAME = process.env.DB_NAME;
+
+// mongoose.connect(MONGO_URL);
+
+
+console.log("hi")
+
+app.listen(3020, () => {
+    console.log(`Server started on port ${3020}`);
+  });
