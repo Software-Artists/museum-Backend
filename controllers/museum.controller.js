18480dba6ce70bd4ef59b8d4bfeb21aa16bd71ac
issasalman <issasalmanhu1996@gmail.com>
2021-09-18 16:14:31 +0300

    created api server

--- controllers/museum.controller.js
+++ controllers/museum.controller.js
@@ -3,41 +3,15 @@
 const axios = require("axios");
 require("dotenv").config();
 
-const { museumModel, Museum } = require("../models/museum.model");
+
 
 const museumData = require("../data/mus.json");
-// console.log(museumData)
-const getMuseum = (request, response) => {
-  const museumName = request.query.name;
 
-  if (museumName) {
-    const museumArr = museumData.filter((item) => {
-     
-      return  item.name.toLowerCase() === museumName.toLowerCase();
-        
-    });
-
-    
-    // console.log(museumArr);
-    let arr1=museumArr.map((mus) => {
-     return new Museum(
-      mus.name,
-      mus.location,
-      mus.event_description,
-      mus.event_description_image,
-      mus.ticket_price,
-      mus.museum_image,
-     
-     
-    );
-  });
-      console.log("Museum",arr1)
-    response.json(arr1);
-  } else {
-    response.json(museumData);
-  }
+const getUser = (request, response) => {
+ 
+  response.json(museumData);
 };
 
 module.exports = {
-  getMuseum,
+  getUser,
 };
